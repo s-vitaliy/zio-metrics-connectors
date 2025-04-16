0c13404724fe7149cee95747fc6428e07204d06f
Vitalii Savitskii <visa@ecco.com>
2025-04-16 15:26:49 +0200

    Rename channelZIO -> channelIP Add channelUDS

--- statsd/src/main/scala/zio/metrics/connectors/statsd/StatsdClient.scala
+++ statsd/src/main/scala/zio/metrics/connectors/statsd/StatsdClient.scala
@@ -1,11 +1,9 @@
 package zio.metrics.connectors.statsd
 
-import java.net.InetSocketAddress
+import java.net.{InetSocketAddress, UnixDomainSocketAddress}
 import java.nio.ByteBuffer
 import java.nio.channels.DatagramChannel
-
 import scala.util.{Failure, Success, Try}
-
 import zio._
 
 trait StatsdClient {
@@ -31,7 +29,7 @@ private[connectors] object StatsdClient {
 
   }
 
-  private def channelZIO(host: String, port: Int): ZIO[Scope, Throwable, DatagramChannel] =
+  private def channelIP(host: String, port: Int): ZIO[Scope, Throwable, DatagramChannel] =
     ZIO.fromAutoCloseable(ZIO.attempt {
       val channel = DatagramChannel.open()
       channel.connect(new InetSocketAddress(host, port))
@@ -39,10 +37,18 @@ private[connectors] object StatsdClient {
       channel
     })
 
+  private def channelUDS(path: String): ZIO[Scope, Throwable, DatagramChannel] =
+    ZIO.fromAutoCloseable(ZIO.attempt {
+      val channel = DatagramChannel.open()
+      channel.connect(UnixDomainSocketAddress.of(path))
+      channel.configureBlocking(false)
+      channel
+    })
+
   private[connectors] def make: ZIO[Scope & StatsdConfig, Nothing, StatsdClient] =
     for {
       config  <- ZIO.service[StatsdConfig]
-      channel <- channelZIO(config.host, config.port).orDie
+      channel <- channelIP(config.host, config.port).orDie
       client   = new Live(channel)
     } yield client
 
