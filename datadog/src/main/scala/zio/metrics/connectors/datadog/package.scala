fd26f295a431c31d68c1178a62df6da31747fee9
Vitalii Savitskii <visa@ecco.com>
2025-05-28 15:59:33 +0200

    Fix incorrect client

--- datadog/src/main/scala/zio/metrics/connectors/datadog/package.scala
+++ datadog/src/main/scala/zio/metrics/connectors/datadog/package.scala
@@ -2,9 +2,9 @@ package zio.metrics.connectors
 
 import zio._
 import zio.internal.RingBuffer
-import zio.metrics.{MetricClient, MetricKey, MetricKeyType}
 import zio.metrics.connectors.internal.MetricsClient
-import zio.metrics.connectors.statsd.{DatagramSocketClient, StatsdClient}
+import zio.metrics.connectors.statsd.{StatsdClient, StatsdConfig}
+import zio.metrics.{MetricClient, MetricKey, MetricKeyType}
 
 package object datadog {
 
@@ -21,7 +21,7 @@ package object datadog {
                             config.entityId,
                             config.sendUnchanged,
                           )
-        clt            <- DatagramSocketClient.make.provideSome[Scope](ZLayer.succeed(config))
+        clt <- StatsdClient.make.provideSome[Scope](ZLayer.succeed(StatsdConfig(config.host, config.port)))
         queue           = RingBuffer.apply[(MetricKey[MetricKeyType.Histogram], Double)](config.maxQueueSize)
         listener        = new DataDogListener(queue)
         _              <- Unsafe.unsafe(unsafe =>
